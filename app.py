1535907d958396c03b442dfc04b4ec3ae006429e
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-06-09 16:02:14 -0400

    copied

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '2: /'
+
+@app.route('/developer-tools', strict_slashes=False) # matches with trailing /
+def developer():
+    return '2: /developer-tools'
+
+@app.route('/developer-tools/sub/path', strict_slashes=False) # matches with trailing /
+def developer_sub_path():
+    return '2: /developer-tools/sub/path'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
