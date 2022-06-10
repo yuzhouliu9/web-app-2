808c3fe021f1fc0e89c3afa094abffe6de2a3854
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-06-10 09:49:50 -0400

    fix

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ def index():
 
 @app.route('/developer', strict_slashes=False) # matches with trailing /
 def developer():
-    return 'web-app-2: /developer-tools'
+    return 'web-app-2: /developer'
 
 @app.route('/developer/sub/path', strict_slashes=False) # matches with trailing /
 def developer_sub_path():
-    return 'web-app-2: /developer-tools/sub/path'
+    return 'web-app-2: /developer/sub/path'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
